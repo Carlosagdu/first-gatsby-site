390f1b07b257938b9ebcdfe288b39ffda853d42f
Carlos Aguilar <caguilar@tiresia.co>
2021-07-26 11:45:46 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My-first-gatsby-site",
+    title: "My-first-gatsby-site-updated",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve:"gatsby-source-filesystem",
+      options:{
+        name:`blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
   ],
 };
